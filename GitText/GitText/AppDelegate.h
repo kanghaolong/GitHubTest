184534a0c584855c29e91a77142d5dcf5d4fd7a5
kanghaolong <185517809@qq.com>
2016-08-05 17:11:01 +0800

    first commit

---
+++ GitText/GitText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitText
+//
+//  Created by KANG on 16/8/5.
+//  Copyright © 2016年 KANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitText/GitText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitText
+//
+//  Created by KANG on 16/8/5.
+//  Copyright © 2016年 KANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
